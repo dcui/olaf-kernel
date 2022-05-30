d2568575bfb9de29447b3d4cbe4ba78ff877b00f
Michal Hocko <mhocko@suse.com>
2022-05-30 14:40:13 +0200

    - KVM: x86/speculation: Disable Fill buffer clear within guests   (bsc#1199650 CVE-2022-21166 CVE-2022-21127 CVE-2022-21123   CVE-2022-21125 CVE-2022-21180). - x86/bugs: Group MDS, TAA & Processor MMIO Stale Data mitigations   (bsc#1199650 CVE-2022-21166 CVE-2022-21127 CVE-2022-21123   CVE-2022-21125 CVE-2022-21180). - x86/speculation: Add a common function for MD_CLEAR mitigation   update (bsc#1199650 CVE-2022-21166 CVE-2022-21127 CVE-2022-21123   CVE-2022-21125 CVE-2022-21180). - x86/speculation/mmio: Add mitigation for Processor MMIO Stale   Data (bsc#1199650 CVE-2022-21166 CVE-2022-21127 CVE-2022-21123   CVE-2022-21125 CVE-2022-21180). - x86/speculation/mmio: Add sysfs reporting for Processor   MMIO Stale Data (bsc#1199650 CVE-2022-21166 CVE-2022-21127   CVE-2022-21123 CVE-2022-21125 CVE-2022-21180). - x86/speculation/mmio: Enable CPU Fill buffer clearing on idle   (bsc#1199650 CVE-2022-21166 CVE-2022-21127 CVE-2022-21123   CVE-2022-21125 CVE-2022-21180). - x86/speculation/mmio: Enumerate Processor MMIO Stale Data bug   (bsc#1199650 CVE-2022-21166 CVE-2022-21127 CVE-2022-21123   CVE-2022-21125 CVE-2022-21180). - x86/speculation/mmio: Reuse SRBDS mitigation for SBDS   (bsc#1199650 CVE-2022-21166 CVE-2022-21127 CVE-2022-21123   CVE-2022-21125 CVE-2022-21180). - x86/speculation/srbds: Update SRBDS mitigation selection   (bsc#1199650 CVE-2022-21166 CVE-2022-21127 CVE-2022-21123   CVE-2022-21125 CVE-2022-21180). - Refresh   patches.suse/powerpc-64s-flush-L1D-after-user-accesses.patch. - Refresh   patches.suse/powerpc-64s-flush-L1D-on-kernel-entry.patch.
    suse-commit: 96526da3a49ae7197f33c8eb9cabd95fa7b7b0b2

--- arch/x86/include/asm/intel-family.h
+++ arch/x86/include/asm/intel-family.h
@@ -53,6 +53,7 @@
 #define INTEL_FAM6_ICELAKE_X		0x6A
 #define INTEL_FAM6_ICELAKE_XEON_D	0x6C
 #define INTEL_FAM6_ICELAKE_DESKTOP	0x7D
+#define INTEL_FAM6_ICELAKE_L		0x7E
 
 /* "Small Core" Processors (Atom) */
 
